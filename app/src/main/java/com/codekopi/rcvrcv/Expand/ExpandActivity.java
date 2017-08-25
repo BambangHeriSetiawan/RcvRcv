714fe2422b7cb330c1596d6ac495fa8cb34fc019
Bambang Heri Setiawan <setiawan.heri.bambang@gmail.com>
2017-08-25 18:54:15 +0700

    failed for expand

--- app/src/main/java/com/codekopi/rcvrcv/Expand/ExpandActivity.java
+++ app/src/main/java/com/codekopi/rcvrcv/Expand/ExpandActivity.java
@@ -1,8 +1,11 @@
 package com.codekopi.rcvrcv.Expand;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.codekopi.rcvrcv.R;
+import com.codekopi.rcvrcv.common.data.AbstractAddRemoveExpandableDataProvider;
+import com.codekopi.rcvrcv.common.fragment.ExampleAddRemoveExpandableDataProviderFragment;
 
 public class ExpandActivity extends AppCompatActivity {
   private static final String FRAGMENT_TAG_DATA_PROVIDER = "data provider";
@@ -11,6 +14,17 @@ public class ExpandActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_expand);
-
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .add(new ExampleAddRemoveExpandableDataProviderFragment(), FRAGMENT_TAG_DATA_PROVIDER)
+          .commit();
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.container, new ExpandFragment(), FRAGMENT_LIST_VIEW)
+          .commit();
+    }
+  }
+  public AbstractAddRemoveExpandableDataProvider getDataProvider() {
+    final Fragment fragment = getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG_DATA_PROVIDER);
+    return ((ExampleAddRemoveExpandableDataProviderFragment) fragment).getDataProvider();
   }
 }
--- app/src/main/java/com/codekopi/rcvrcv/Expand/ExpandFragment.java
+++ app/src/main/java/com/codekopi/rcvrcv/Expand/ExpandFragment.java
@@ -1,29 +1,99 @@
 package com.codekopi.rcvrcv.Expand;
 
 import android.content.Context;
+import android.graphics.drawable.NinePatchDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codekopi.rcvrcv.R;
+import com.codekopi.rcvrcv.common.data.AbstractAddRemoveExpandableDataProvider;
+import com.h6ah4i.android.widget.advrecyclerview.animator.GeneralItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.animator.RefactoredDefaultItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.decoration.ItemShadowDecorator;
+import com.h6ah4i.android.widget.advrecyclerview.decoration.SimpleListDividerDecorator;
+import com.h6ah4i.android.widget.advrecyclerview.expandable.RecyclerViewExpandableItemManager;
 
 public class ExpandFragment extends Fragment {
+  private static final String SAVED_STATE_EXPANDABLE_ITEM_MANAGER = "RecyclerViewExpandableItemManager";
 
+  private RecyclerView mRecyclerView;
+  private RecyclerView.LayoutManager mLayoutManager;
+  private ExpandAdapter mAdapter;
+  private RecyclerView.Adapter mWrappedAdapter;
+  private RecyclerViewExpandableItemManager mRecyclerViewExpandableItemManager;
 
   public ExpandFragment() {
-    // Required empty public constructor
+    super();
+    setHasOptionsMenu(true);
   }
 
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_expand, container, false);
   }
 
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    //noinspection ConstantConditions
+    mRecyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+    mLayoutManager = new LinearLayoutManager(getContext());
 
+    final Parcelable eimSavedState = (savedInstanceState != null) ? savedInstanceState.getParcelable(SAVED_STATE_EXPANDABLE_ITEM_MANAGER) : null;
+    mRecyclerViewExpandableItemManager = new RecyclerViewExpandableItemManager(eimSavedState);
+
+    // Expand all group items by default. This method must be called before creating a wrapper adapter.
+    //
+    // FYI: AbstractExpandableItemAdapter.getInitialGroupExpandedState() can also be used if you
+    // need fine control of initial group items' state.
+    mRecyclerViewExpandableItemManager.setDefaultGroupsExpandedState(true);
+
+    //adapter
+    final ExpandAdapter myItemAdapter = new ExpandAdapter(mRecyclerViewExpandableItemManager, getDataProvider());
+
+    mAdapter = myItemAdapter;
+
+    mWrappedAdapter = mRecyclerViewExpandableItemManager.createWrappedAdapter(myItemAdapter);       // wrap for expanding
+    final GeneralItemAnimator animator = new RefactoredDefaultItemAnimator();
+
+    // Change animations are enabled by default since support-v7-recyclerview v22.
+    // Need to disable them when using animation indicator.
+    animator.setSupportsChangeAnimations(false);
+
+    mRecyclerView.setLayoutManager(mLayoutManager);
+    mRecyclerView.setAdapter(mWrappedAdapter);  // requires *wrapped* adapter
+    mRecyclerView.setItemAnimator(animator);
+    mRecyclerView.setHasFixedSize(false);
+
+    // additional decorations
+    //noinspection StatementWithEmptyBody
+    if (supportsViewElevation()) {
+      // Lollipop or later has native drop shadow feature. ItemShadowDecorator is not required.
+    } else {
+      mRecyclerView.addItemDecoration(new ItemShadowDecorator((NinePatchDrawable) ContextCompat
+          .getDrawable(getContext(), R.drawable.material_shadow_z1)));
+    }
+    mRecyclerView.addItemDecoration(new SimpleListDividerDecorator(ContextCompat.getDrawable(getContext(), R.drawable.list_divider_h), true));
+
+    mRecyclerViewExpandableItemManager.attachRecyclerView(mRecyclerView);
+  }
+
+  private boolean supportsViewElevation() {
+    return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
+  }
+  public AbstractAddRemoveExpandableDataProvider getDataProvider() {
+    return ((ExpandActivity) getActivity()).getDataProvider();
+  }
 }
